745e1125a8c421b3d8823a13c1a3fcb2638625fc
Mathew Petronilho <petromathew@gmail.com>
2021-08-27 17:43:22 -0400

    Heap sort updated

--- Sorts/heap_sort.py
+++ Sorts/heap_sort.py
@@ -1,5 +1,10 @@
+from priority_queues import *
+
 def heap_sort(L):
 	temp = []
 	max_heap = Pri_Queue_heap(L)
 	for i in range(len(L)):
-		temp.append()
+		temp.insert(0, max_heap.remove_max())
+
+	for j in range(len(L)):
+		L[j] = temp[j]
